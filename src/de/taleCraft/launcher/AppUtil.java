24ad58fcbafda25f757bfd3d01e2b273801544d8
Lars K <lars195@yahoo.de>
2014-01-07 22:58:05 +0100

    Created AppUtil class. See commit-detail.
    - Exception-less sleep method.
    - URLConnection 'input-stream avaible' method.
    - Reflection->getFieldContent method.

---
+++ src/de/taleCraft/launcher/AppUtil.java
@@ -0,0 +1,73 @@
+
+package de.taleCraft.launcher;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.HttpURLConnection;
+import java.net.JarURLConnection;
+import java.net.URLConnection;
+
+import sun.net.www.protocol.file.FileURLConnection;
+
+public class AppUtil {
+	
+	public static final void sleep(long time)
+	{
+		try
+		{
+			Thread.sleep(time);
+		}
+		catch (InterruptedException e)
+		{
+			// Do nothing.
+		}
+	}
+
+	public static boolean isAvaible(URLConnection connection) {
+		
+		if(connection instanceof JarURLConnection)
+			return true;
+		
+		if(connection instanceof HttpURLConnection)
+			try {
+				return ((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_OK;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		
+		if(connection instanceof FileURLConnection)
+			return true;
+		
+		return false;
+	}
+	
+	public static final Object getFieldContent(Class<?> objectClass, Object object, String fieldName)
+	{
+		System.out.println("Reflection Access: " + object.getClass() + " @--> " + fieldName);
+		try {
+			Field field = objectClass.getDeclaredField(fieldName);
+			
+			field.setAccessible(true);
+			
+			try {
+				return field.get(object);
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+				return null;
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+				return null;
+			}
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+			return null;
+		} catch (SecurityException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	
+}
