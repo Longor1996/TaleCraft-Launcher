48724b47d020e6d696dce5006db70dede93b663e
Lars K <lars195@yahoo.de>
2014-01-07 22:58:39 +0100

    Created AppConstants class, and added constants.

---
+++ src/de/taleCraft/launcher/AppConstants.java
@@ -0,0 +1,15 @@
+package de.taleCraft.launcher;
+
+import java.awt.Color;
+
+public class AppConstants {
+	// Application (APP)
+	
+	
+	// Network (NET)
+	public static final String NET_VersionInfoURL = "file:///C:/Develope/delocalized_projects/TaleCraft-Launcher/try/serverside/";
+	
+	// Swing (UI)
+	public static final Color NULL = new Color(0f,0f,0f,0f);
+	
+}
