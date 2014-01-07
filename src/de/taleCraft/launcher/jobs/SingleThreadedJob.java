a6928c122add893ef6342371760ad83b261142fc
Lars K <lars195@yahoo.de>
2014-01-07 16:32:50 +0100

    ???: Job System, Error/Failure Handling

--- src/de/taleCraft/launcher/jobs/SingleThreadedJob.java
+++ src/de/taleCraft/launcher/jobs/SingleThreadedJob.java
@@ -11,7 +11,22 @@ public abstract class SingleThreadedJob implements Runnable {
 	@Override
 	public final void run() {
 		System.out.println("Execution of Job -> " + this.jobName);
-		this.execute();
+		
+		try
+		{
+			this.execute();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Job Failed -> " + this.jobName);
+			System.out.println("Job Fail Reason -> " + e.getLocalizedMessage());
+			e.printStackTrace();
+			
+			return;
+		}
+		
+		System.out.println("Finished Job -> " + this.jobName);
+		return;
 	}
 
 	public abstract void execute();
