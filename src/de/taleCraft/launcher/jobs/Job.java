3066ae16b30517b916d0674056749da188ed51c0
Lars K <lars195@yahoo.de>
2014-01-11 18:28:20 +0100

    Continued work on the Job-System, and a Job.
    - Added everything probably needed for a good multithreaded Job-System.
    - Continued work on the initialization Job of the Launcher.
    
    I should code a bit faster, or else this will never get done.

--- src/de/taleCraft/launcher/jobs/Job.java
+++ src/de/taleCraft/launcher/jobs/Job.java
@@ -1,36 +1,96 @@
 package de.taleCraft.launcher.jobs;
 
-public abstract class SingleThreadedJob implements Runnable {
+public abstract class Job<T> implements Runnable {
 	public final String jobName;
+	private boolean finished;
+	private boolean success;
+	private T returnObject;
 	
-	public SingleThreadedJob(String name)
+	public Job(String name)
 	{
 		this.jobName = name;
+		this.finished = false;
 	}
 	
 	@Override
 	public final void run() {
-		System.out.println("Execution of Job -> " + this.jobName);
+		// Reset, Job Object Data
+		this.returnObject = null;
+		this.finished = false;
+		this.success = false;
+		
+		// ---------------------- EXECUTION ---------------------- //
+		System.out.println("[JOB] Execution of Job -> " + this.jobName);
+		
+		Object jobFailureReturnObject = null;
 		
 		try
 		{
-			this.execute();
+			jobFailureReturnObject = this.execute();
 		}
-		catch(Exception e)
+		catch(Throwable e)
 		{
-			System.out.println("Job Failed -> " + this.jobName);
-			System.out.println("Job Fail Reason -> " + e.getLocalizedMessage());
-			e.printStackTrace();
-			
-			return;
+			jobFailureReturnObject = e;
 		}
 		
-		System.out.println("Finished Job -> " + this.jobName);
+		if(jobFailureReturnObject == null)
+		{
+			// ---------------------- SUCCESS ---------------------- //
+			System.out.println("[JOB] Finished Job -> " + this.jobName);
+			
+			if(this.returnObject != null)
+			{
+				System.out.println("[JOB] Job Result: " + this.returnObject);;
+			}
+			
+			this.success = true;;
+		}
+		else
+		{
+			// ---------------------- FAILURE ---------------------- //
+			if(jobFailureReturnObject instanceof Exception)
+			{
+				System.out.println("[JOB] The Job '"+this.jobName+"' failed because of an Exception.");
+				System.out.println("[JOB] Job Failure Exception -> " + ((Exception) jobFailureReturnObject).getLocalizedMessage());
+				((Exception) jobFailureReturnObject).printStackTrace();
+			}
+			else
+			{
+				System.out.println("[JOB] The Job '"+this.jobName+"' failed because it reported a error object.");
+				System.out.println("[JOB] Job Error Object Class -> " + jobFailureReturnObject.getClass().getName());
+				System.out.println("[JOB] Job Error Object -> " + jobFailureReturnObject);
+			}
+			
+			this.success = false;;
+		}
+		
+		this.finished = true;
 		return;
 	}
-
-	public abstract void execute();
 	
+	public abstract Object execute();
 	
+	public boolean isFinished()
+	{
+		return this.finished;
+	}
+	
+	public boolean isSuccess()
+	{
+		if(!this.finished)
+			throw new IllegalStateException("Execution is not yet finished!");
+		
+		return this.success;
+	}
+	
+	public void setResult(T returnObject)
+	{
+		this.returnObject = returnObject;
+	}
+	
+	public T getResult()
+	{
+		return this.returnObject;
+	}
 	
 }
--- src/de/taleCraft/launcher/jobs/STJ_Initialize.java
+++ src/de/taleCraft/launcher/jobs/STJ_Initialize.java
@@ -1,6 +1,6 @@
 package de.taleCraft.launcher.jobs;
 
-import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -10,11 +10,14 @@ import java.net.URLConnection;
 import javax.swing.JLabel;
 import javax.swing.SpringLayout;
 
+import org.apache.commons.io.FileUtils;
+
 import de.taleCraft.launcher.AppConstants;
 import de.taleCraft.launcher.AppUtil;
 import de.taleCraft.launcher.LauncherFrame;
+import de.taleCraft.launcher.TaleCraftLauncher;
 
-public class STJ_Initialize extends SingleThreadedJob {
+public class STJ_Initialize extends Job<EnumUpdateCheckResult> {
 	
 	public STJ_Initialize()
 	{
@@ -22,7 +25,7 @@ public class STJ_Initialize extends SingleThreadedJob {
 	}
 	
 	@Override
-	public void execute() {
+	public Object execute() {
 		
 		LauncherFrame frame = LauncherFrame.INSTANCE;
 		
@@ -41,35 +44,123 @@ public class STJ_Initialize extends SingleThreadedJob {
 		
 		frame.window.repaint();
 		
+		boolean indexFileDownloadSuccess = false;
+		File dowloadIndexLocalFile = new File(TaleCraftLauncher.launcher.workingDirectory, AppConstants.IO_DownloadIndexFileName);
+		File localVersionFile = new File(TaleCraftLauncher.launcher.workingDirectory, AppConstants.IO_VersionFileName);
+		FileUtils.deleteQuietly(dowloadIndexLocalFile);
+		AppUtil.sleep(100L);
+		
 		try {
 			URL url = new URL(AppConstants.NET_VersionInfoURL);
 			URLConnection connection = url.openConnection();
 			
-			
-			
 			if(AppUtil.isAvaible(connection))
 			{
 				InputStream inputStream = connection.getInputStream();
-				System.err.println("Success: " + connection + " ::> " + inputStream);
-				
-				byte[] a = (byte[]) AppUtil.getFieldContent(ByteArrayInputStream.class, inputStream, "buf");
-				
-				System.out.println("["+a.length+"] " + new String(a));
-				
-				;
+				FileUtils.copyInputStreamToFile(inputStream, dowloadIndexLocalFile);
+				inputStream.close();
+				indexFileDownloadSuccess = true;
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			indexFileDownloadSuccess = false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			indexFileDownloadSuccess = false;
+		}
+		
+		// Load the current Version Information!
+		String currentVersion = null;
+		try {
+			currentVersion = FileUtils.readFileToString(localVersionFile);
+		} catch (IOException e) {
+			System.out.println("[Error] Failed to read local Version-File: " + e.getLocalizedMessage());
+			currentVersion = null;
+		}
+		
+		// Load the downloadIndex!
+		DownloadIndexInfo downloadIndexInfo = new DownloadIndexInfo(dowloadIndexLocalFile);
+		
+		EnumUpdateCheckResult result = this.compareVersion(currentVersion, downloadIndexInfo.versionString);
+		
+		// Here comes the true nightmare!
+		// Do stuff depending on the EnumUpdateCheckResult!
+		
+		System.out.println("result: " + result);
+		
+		this.setResult(result);
+		return null;
+	}
+	
+	public EnumUpdateCheckResult compareVersion(String current, String downloadIndexVersion)
+	{
+		if((current == null) && (downloadIndexVersion != null))
+			return EnumUpdateCheckResult.NO_LOCAL_ONLINE_AVAIBLE;
+		
+		if((current != null) && (downloadIndexVersion == null))
+			return EnumUpdateCheckResult.LOCAL_AVAIBLE_ONLINE_NOT;
+		
+		if((current == null) && (downloadIndexVersion == null))
+			return EnumUpdateCheckResult.NO_VERSION_AVAIBLE_LOCAL_NOR_ONLINE;
+		
+		if((current != null) && (downloadIndexVersion != null))
+			; // continue to compare
+		
+		System.out.println("compareVersion C = " + current);
+		System.out.println("compareVersion D = " + downloadIndexVersion);
+		
+		// Convert strings into binary representations
+		// split step
+		String[] strC = current.split(".");
+		String[] strD = downloadIndexVersion.split(".");
+		
+		// malloc step
+		int[] intC = new int[strC.length];
+		int[] intD = new int[strD.length];
+		
+		// convert step
+		for(int i = 0; i < strC.length; i++)
+			intC[i] = Integer.valueOf(strC[i]);
+		for(int i = 0; i < strD.length; i++)
+			intD[i] = Integer.valueOf(strD[i]);
+		
+		// Compare the two int arrays, and check which one is bigger.
+		for(int i = 0; i < strC.length; i++)
+		{
+			// fetch step
+			int C = intC[i];
+			int D = intD[i];
+			
+			// comparison step
+			if(C > D)
+			{
+				; // C is newer.
+				return EnumUpdateCheckResult.CURRENT_IS_NEWER;
+			}
+			else if(D > C)
+			{
+				; // D is newer.
+				return EnumUpdateCheckResult.DOWNLOAD_IS_NEWER;
 			}
 			else
 			{
-				System.err.println("Error X: " + connection);;
+				; // Same number, check if this is the last index!
+				if(i == (strC.length-1))
+				{
+					; // The versions are completely the same.
+					return EnumUpdateCheckResult.SAME_VERSION;
+				}
+				else
+				{
+					; // Same number, check the next.
+					continue;
+				}
 			}
-			// connection.
-			
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 		
+		// This should never be reached!
+		// error return step
+		return EnumUpdateCheckResult.NO_VERSION_AVAIBLE_LOCAL_NOR_ONLINE;
 	}
 	
 }
