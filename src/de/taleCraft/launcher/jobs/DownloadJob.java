c9e620baa491fff8d2b6fab162da93d846c34556
Lars K <lars195@yahoo.de>
2014-01-11 23:26:57 +0100

    Implmented a basic DownloadJob class.

---
+++ src/de/taleCraft/launcher/jobs/DownloadJob.java
@@ -0,0 +1,37 @@
+package de.taleCraft.launcher.jobs;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+
+public class DownloadJob extends Job<File>{
+	String source;
+	File destination;
+	
+	public DownloadJob(String name, String source, File file) {
+		super("download-thread:"+name);
+		this.source = source;
+		this.destination = file;
+	}
+	
+	@Override
+	public Object execute() throws Throwable {
+		this.setResult(this.destination);
+		
+		try {
+			FileUtils.copyURLToFile(new URL(this.source), this.destination, 5000, 2000);
+		} catch (IOException e) {
+			// Oh, the download failed... the job failed.
+			throw e;
+		}
+		
+		// We did it! We successfully downloaded the File!
+		
+		return null;
+	}
+	
+	
+	
+}
