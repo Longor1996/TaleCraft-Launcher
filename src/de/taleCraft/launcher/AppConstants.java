5faf6a58c22509e655a57ba2aad77e626c3fa49d
Lars K <lars195@yahoo.de>
2014-01-11 18:22:48 +0100

    Added new constants to AppConstants.

--- src/de/taleCraft/launcher/AppConstants.java
+++ src/de/taleCraft/launcher/AppConstants.java
@@ -6,8 +6,12 @@ public class AppConstants {
 	// Application (APP)
 	
 	
-	// Network (NET)
-	public static final String NET_VersionInfoURL = "file:///C:/Develope/delocalized_projects/TaleCraft-Launcher/try/serverside/";
+	// Network (NET) && InputOutput (IO/NIO)
+	// NET is Internet.
+	// IO is local.
+	public static final String NET_VersionInfoURL = "file:///C:/Develope/delocalized_projects/TaleCraft-Launcher/try/serverside/downloadIndex.txt";
+	public static final String IO_VersionFileName = "version.txt";
+	public static final String IO_DownloadIndexFileName = "downloadIndex.txt";
 	
 	// Swing (UI)
 	public static final Color NULL = new Color(0f,0f,0f,0f);
